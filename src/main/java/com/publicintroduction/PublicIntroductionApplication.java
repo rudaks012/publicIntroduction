a058b437b0042c1e2c1899f9b28b3c5aeefba4c5
gyeongman <rudaks012@naver.com>
2023-03-12 21:09:09 +0900

    Feat : React + SpringBoot 게시판 만들기

---
+++ src/main/java/com/publicintroduction/PublicIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.publicintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicIntroductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/publicintroduction/PublicIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
